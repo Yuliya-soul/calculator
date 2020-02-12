157f00651c527debe787aa0ff23648ea64e0399b
Your Name <you@example.com>
2020-02-13 00:25:14 +0300

    feat :upload calculator

---
+++ src/InfoCard.js
@@ -0,0 +1,16 @@
+
+const InfoCard = [
+   
+    {
+       MSRP: 35000,
+       VehicleName: "Toyota",
+      MonthlyPayment: 368,
+      taxes:100,
+      DealerPhoneNumber: "617-564-3254",
+      DealerRating:100,
+ 
+    },
+  
+  ];
+
+export {InfoCard};
